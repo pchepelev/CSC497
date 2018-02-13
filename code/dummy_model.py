77d6df86076f996680cdd406f53e3e388a078492
Bill Bird <34454419+billbird@users.noreply.github.com>
2018-02-13 15:33:33 -0800

    Add model framework mockups

---
+++ code/dummy_model.py
@@ -0,0 +1,8 @@
+
+
+
+import numpy
+
+
+def generate_roads( x_size, y_size, data_layers, starting_point ):
+	return numpy.zeros( (x_size, y_size) )
---
+++ code/run_model.py
@@ -0,0 +1,24 @@
+
+
+
+import numpy
+import json
+import sys
+
+import dummy_model
+
+if len(sys.argv) < 2:
+	print('Usage: %s <config file (JSON)>'%sys.argv[0])
+	sys.exit()
+
+config_file_name = sys.argv[1]
+
+#Get stuff from the JSON file
+
+
+#Eventually...
+#roads = dummy_model.generate_roads( size_x, size_y, data_layers, (starting_point_x, starting_point_y))
+
+#Now analyse the result
+
+
