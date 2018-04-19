cc6bf38694bc4e795fb5d021af71a1a3c72d64bf
Peter Chepelev <17349985+pchepelev@users.noreply.github.com>
2018-04-19 02:46:27 -0700

    choosy tiebreakers & alg. select

--- code/run_model.py
+++ code/run_model.py
@@ -6,8 +6,8 @@ import util
 
 if __name__ == '__main__':
 	
-	if len(sys.argv) < 5:
-		print('Usage: %s <config file (JSON)> <search radius> <How often to save intermediate grids (0: don\'t save)> <algorithm choice>'%sys.argv[0])
+	if len(sys.argv) < 6:
+		print('Usage: %s <config file (JSON)> <search radius> <How often to save intermediate grids (0: don\'t save)> <algorithm choice> <output filename>'%sys.argv[0])
 		sys.exit()
 
 	file_name = sys.argv[1]
@@ -26,4 +26,4 @@ if __name__ == '__main__':
 	#get grid file
 	roads = models.run_algorithm(name, layers,search_radius,access_point, save_period, choice)
 	print("model created. saving file...")
-	util.saveFile(roads, 'modeled_road', layers)
+	util.saveFile(roads, sys.argv[5], layers)
